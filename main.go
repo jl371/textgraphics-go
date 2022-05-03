07ac52f81d8d7f597eb69b2bb18410635458b7e1
jlucid <jlucid@uwyo.edu>
2022-05-03 19:06:18 -0400

    Added and tested drawline function, movecursor

--- main.go
+++ main.go
@@ -59,12 +59,154 @@ func setPen(b bool) {
 func removePixel(w int, z int) {
 	layer1.sc[w][z] = false;
 }
+func movecursor(w int, z int) {
+	x = w
+	y = z
+}
+func drawLine(length int, orientation int) {
+	plotPixel(x,y)
+	switch (orientation) {
+	case 0:
+		//draw line upwards
+		for i:=1; i < length; i++ {
+			h := y - i
+			if (h < 0 || h > 30) {
+				break
+			} else {
+				if pen == true {
+					layer1.sc[x][h] = true
+				} else {
+					layer1.sc[x][h] = false
+				}
+				
+			}
+		}
+		break
+	case 1:
+		//diagonal line to top right
+		for i := 1; i < length; i++ {
+                h := y - i
+                j := x + i
+                if (h < 0 || h > 30) || (j < 0 || j > 80) {
+					break 
+				} else {
+					if pen == true {
+						layer1.sc[j][h] = true
+					} else {
+						layer1.sc[j][h] = false
+					}
+				}
+            }
+            break
+	case 2:
+		for i := 1; i < length; i++ {
+                j := x + i;
+                if ((j < 0 || j > 80)) {
+					break
+				} else { 
+					if pen == true {
+						layer1.sc[j][y] = true
+					} else {
+						layer1.sc[j][y] = false
+					}
+				}
+            }
+            break
+	case 3:
+		//diagonal line to the bottom right
+		for i := 1; i < length; i++ {
+			h := y + i
+			j := x + i
+			if ((h < 0 || h > 30) || (j < 0 || j > 80)) {
+				break
+			} else {
+				if pen == true {
+					layer1.sc[j][h] = true
+				} else {
+					layer1.sc[j][h] = false
+				}
+			}
+		}
+		break
+	case 4:
+		//draw line downwards, starting at x, y
+		for i := 1; i < length; i++ {
+			h := y + i
+			if (h < 0 || h > 30) { 
+				break 
+			} else { 
+				if pen == true {
+					layer1.sc[x][h] = true
+				} else {
+					layer1.sc[x][h] = false
+				}
+			}
+		}
+		break;
+	case 5:
+		//diagonal line to the bottom left
+		for i := 1; i < length; i++ {
+			h := y + i
+			j := x - i
+			if ((h < 0 || h > 30) || (j < 0 || j > 80)) { 
+				break; 
+			} else {
+				if pen == true {
+					layer1.sc[j][h] = true
+				} else {
+					layer1.sc[j][h] = false
+				}
+			} 
+		}
+		break;
+	case 6:
+		for i := 1; i < length; i++ {
+                j := x - i
+                if ((j < 0 || j > 80)) {
+					break
+				} else { 
+					if pen == true {
+						layer1.sc[j][y] = true
+					} else {
+						layer1.sc[j][y] = false
+					}
+				}
+            }
+        break
+	case 7:
+		for i := 1; i < length; i++ {
+                h := y - i
+                j := x - i
+                if ((h < 0 || h > 30) || (j < 0 || j > 80)) {
+					break
+				} else { 
+					if pen == true {
+						layer1.sc[j][h] = true
+					} else {
+						layer1.sc[j][h] = false
+					}
+				}
+				
+            }
+            break
+	}
+
+
+}
 
 func main(){
 	log.Println("things")
 	layer1.sc = InitScreen()
 	plotPixel(0,20)
-	removePixel(0,20)
+	movecursor(10,10)
+	drawLine(5,0)
+	drawLine(5,1)
+	drawLine(5,2)
+	drawLine(5,3)
+	drawLine(5,4)
+	drawLine(5,5)
+	drawLine(5,6)
+	drawLine(5,7)
 	DisplayMe()
 
 }
